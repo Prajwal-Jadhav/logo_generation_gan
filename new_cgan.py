83906adf5255d258bdbb22d2bea335825d426138
Prajwal-Jadhav <jadhavv933@gmail.com>
2023-03-14 11:19:09 +0530

    passing tuple to the resize method instead int

--- new_cgan.py
+++ new_cgan.py
@@ -37,7 +37,7 @@ img_shape = (opt.channels, opt.img_size, opt.img_size)
 cuda = True if torch.cuda.is_available() else False
 
 transform = transforms.Compose(
-            [transforms.Resize(opt.img_size), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
+            [transforms.Resize((opt.img_size, opt.img_size)), transforms.ToTensor(), transforms.Normalize([0.5], [0.5])]
         )
 
 class LabeledDataset(torch.utils.data.Dataset):
